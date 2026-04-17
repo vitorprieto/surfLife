0bf7c0f6bf03d2c4f343e1996bf9992a04f5e8ea
Original Truta <vitor.originaltruta@gmail.com>
2026-04-17 10:18:17 -0300

    commit inicial

---
+++ src/main/java/com/br/originalTruta/surfLife/SurfLifeApplication.java
@@ -0,0 +1,13 @@
+package com.br.originalTruta.surfLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurfLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurfLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/originalTruta/surfLife/SurfLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.originalTruta.surfLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurfLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
